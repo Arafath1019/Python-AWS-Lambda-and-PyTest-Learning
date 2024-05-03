31c0f3f561c81762f2b938b85ec691df275a9e11
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-05-03 17:05:55 +0600

    feat: parameterize pytest

--- PYTEST TUTORIAL - PyTest for Absolute Beginners/LearnPyTest/conftest.py
+++ PYTEST TUTORIAL - PyTest for Absolute Beginners/LearnPyTest/conftest.py
@@ -8,10 +8,23 @@ import pytest
 #     yield
 #     print("Close Browser")
 
-# @pytest.fixture(scope="session", autouse=True)
-# def setUp():
-#     print("Launch browser")
-#     print("Login")
-#     print("Browse products")
-#     yield
-#     print("Close Browser")
+@pytest.fixture(scope="session", autouse=True)
+def setUp(browser):
+    if browser == "chrome":
+        print("Launch chrome browser")
+    if browser == "firefox":
+        print("Launch firefox browser")
+    if browser == "edge":
+        print("Launch edge browser")
+    print("Launch browser")
+    print("Login")
+    print("Browse products")
+    yield
+    print("Close Browser")
+    
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture(scope="session", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
