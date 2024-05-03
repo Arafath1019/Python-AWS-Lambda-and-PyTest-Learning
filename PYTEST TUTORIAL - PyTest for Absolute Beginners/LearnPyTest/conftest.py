57c401338dd2c4791cdad8cac9c00e05fe3aff8f
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-05-03 16:02:04 +0600

    feat: conftest in pytest

---
+++ PYTEST TUTORIAL - PyTest for Absolute Beginners/LearnPyTest/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+# @pytest.fixture(autouse=True, scope="session")
+# def setUp():
+#     print("Launch browser")
+#     print("Login")
+#     print("Browse products")
+#     yield
+#     print("Close Browser")
+
+@pytest.fixture(scope="session", autouse=True)
+def setUp():
+    print("Launch browser")
+    print("Login")
+    print("Browse products")
+    yield
+    print("Close Browser")
--- PYTEST TUTORIAL - PyTest for Absolute Beginners/LearnPyTest/test_checkout.py
+++ PYTEST TUTORIAL - PyTest for Absolute Beginners/LearnPyTest/test_checkout.py
@@ -1,18 +1,16 @@
 import pytest
 
-@pytest.fixture()
-def setUp():
-    print("Launch browser")
-    print("Login")
-    print("Browse products")
-    yield
-    print("Close Browser")
+# def testLogin(setUp):
+#     print("Login Successful")
+    
+# def testLogoff(setUp):
+#     print("Logoff Successful")
 
-def testLogin(setUp):
+def testLogin():
     print("Login Successful")
     
-def testLogoff(setUp):
+def testLogoff():
     print("Logoff Successful")
 
 def testCalculation():
-    assert 2+2 == 6
+    assert 2+2 == 4
